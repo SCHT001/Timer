6965857e0e2f07dbec3da9ac0448fb52b48a03a8
Sachet Subedi <sachetsubedi001@gmail.com>
2023-06-02 19:07:49 +0545

    first

---
+++ Script.js
@@ -0,0 +1,104 @@
+const enterBtn=document.getElementById('enterBtn');
+enterBtn.addEventListener('click',getInput);
+
+
+var hrs,min,sec;
+
+function getInput()
+{
+    var hrsInput=document.getElementById('hours');
+    hrs=parseInt(hrsInput.value);
+    var minInput=document.getElementById('minutes');
+    min=parseInt(minInput.value);
+    var secInput=document.getElementById('seconds');
+    sec=parseInt(secInput.value);
+
+    var displayValue=document.getElementById('display');
+// To format 1 to 01..
+    var h=formatter(hrs);
+    var m=formatter(min);
+    var s=formatter(sec);
+   
+//End of formater
+
+    displayValue.innerHTML= h+" :: " + m +" :: "+ s;
+}
+
+function formatter(x)
+{
+    if(x<10)
+    {
+        return ("0"+x);
+    }
+    else
+    {
+        return x;
+    }
+}
+var running=false;
+var timer;
+const startBtn=document.getElementById('start');
+startBtn.addEventListener('click',start);
+function start()
+{
+    if(!running && (hrs>0 || min> 0 ||sec >0)){
+    timer=setInterval(clockStarter,1000);
+    running=true;
+    }
+}
+
+function clockStarter()
+{
+    
+    
+//condition to change minute and hrs
+    if(sec==0)
+    {
+        min--;
+        sec=60;
+        if(min==0 && hrs>0)
+        {
+            
+            min=59;
+            if(hrs>0)
+            {
+                hrs--;
+            }
+        }
+        
+    }
+    if(min==0 && hrs>0)
+    {
+        min=60;
+        hrs--;
+    }
+    sec--;
+    var displayValue=document.getElementById('display');
+    displayValue.innerHTML=formatter(hrs)+" :: "+formatter(min)+" :: "+formatter(sec);
+}
+ //clock reseter
+
+ var resetBtn=document.getElementById('reset');
+ resetBtn.addEventListener('click',function()
+ {
+    displayValue=document.getElementById('display');
+    displayValue.innerHTML="00 :: 00 :: 00";
+    running=false;
+    hrs=0;
+    min=0;
+    sec=0;
+    clearInterval(timer);
+ })
+//clear input
+
+var clearBtn=document.getElementById('clearBtn');
+clearBtn.addEventListener('click',function()
+{
+    var hrsInput=document.getElementById('hours');
+    hrsInput.value=00;
+    var minInput=document.getElementById('minutes');
+    minInput.value=00;
+    var secInput=document.getElementById('seconds');
+    secInput.value=00;
+
+})
